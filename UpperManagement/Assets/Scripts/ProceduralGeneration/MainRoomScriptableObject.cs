027c73cdb16d3181d554419ebf0a3d1e4babe004
LunarHackett003 <LunarHackett003@users.noreply.github.com>
2022-11-01 14:38:54 +0000

    Added room generation!
    Woo! Room generation has been

---
+++ UpperManagement/Assets/Scripts/ProceduralGeneration/MainRoomScriptableObject.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Main Room Container", menuName = "Room Containers/Main Rooms")]
+public class MainRoomScriptableObject : ScriptableObject
+{
+
+    public List<GameObject> roomObject;
+
+
+
+
+}
