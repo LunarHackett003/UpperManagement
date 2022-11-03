b8bec8bfbdd46f19683dbe4f94feb563b39ac782
LunarHackett003 <61294652+LunarHackett003@users.noreply.github.com>
2022-11-03 16:54:31 +0000

    Player Animation Setter added

--- UpperManagement/Assets/Scripts/Characters/CharacterAnimator.cs
+++ UpperManagement/Assets/Scripts/Characters/CharacterAnimator.cs
@@ -9,12 +9,18 @@ public class CharacterAnimator : MonoBehaviour
 
     private void Start()
     {
-        
+        anim= character.GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
-        
+        anim.SetBool("light", character.ivars.lightInput);
+        anim.SetBool("moving", character.ivars.moveInput != 0);
+        anim.SetBool("heavy", character.ivars.heavyInput);
+        anim.SetBool("ranged", character.ivars.rangedInput);
+        anim.SetBool("jumping", character.ivars.verticalInput >= 0.2f);
+
+    
     }
 
 }
