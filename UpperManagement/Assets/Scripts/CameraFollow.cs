3763d9b01809b2b07e4f70f29598c7529818db04
LunarHackett003 <LunarHackett003@users.noreply.github.com>
2022-11-08 15:23:15 +0000

    stupid meta stuff, game manager added.
    >:0

--- UpperManagement/Assets/Scripts/CameraFollow.cs
+++ UpperManagement/Assets/Scripts/CameraFollow.cs
@@ -8,6 +8,10 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] Vector3 offset;
     [SerializeField] float lerpSpeed;
     [SerializeField] float lookaheadDivider;
+    private void Start()
+    {
+        cameraFollow = GameObject.FindGameObjectWithTag("Player").GetComponent<Character>();
+    }
     private void FixedUpdate()
     {
         transform.position = Vector3.Lerp(transform.position, cameraFollow.transform.position + offset + ((Vector3)cameraFollow.rb.velocity / lookaheadDivider), Time.fixedDeltaTime * lerpSpeed);
