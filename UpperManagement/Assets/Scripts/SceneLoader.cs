a7c321cb00946406d6d38ed73218d5625ee00215
LunarHackett003 <LunarHackett003@users.noreply.github.com>
2022-11-08 16:24:11 +0000

    dumb ai fix

--- UpperManagement/Assets/Scripts/SceneLoader.cs
+++ UpperManagement/Assets/Scripts/SceneLoader.cs
@@ -1,23 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
     [SerializeField] bool loadOnStart;
 
     [SerializeField] int sceneIndex;
+    [SerializeField] int fadeTime;
+
+
     private void Start()
     {
         if (loadOnStart)
         {
-
+            LoadNextScene();
         }
     }
 
 
     public void LoadNextScene()
     {
+        SceneManager.LoadScene(sceneIndex);
+    }
 
+
+    IEnumerator ScreenFade()
+    {
+        float currentFade = 0f;
+
+        while (currentFade < fadeTime)
+        {
+            currentFade += Time.fixedDeltaTime;
+
+
+            yield return null;
+        }
+        LoadNextScene();
+        yield return null;
     }
 }
